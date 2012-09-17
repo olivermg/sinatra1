ffd126f571545f1bf4cdfbd5aebe99bb602c15f2
Oliver Wegner <void1976@gmail.com>
2012-09-17 22:15:41 +0200

    Playing around with ICanHaz.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,8 +3,9 @@ Custom = {
     callback1: function(data) {
 	//alert( "result: " + data["result"] );
 	//$("h1")[0].innerHTML = "result: " + data['result'];
-	var h1 = $("h1")[0];
-	h1.innerHTML = Mustache.render( h1.innerHTML, data );
+	//var h1 = $("h1")[0];
+	//h1.innerHTML = Mustache.render( h1.innerHTML, data );
+	$("h1").html( ich.icanhaz1( data ) );
     },
 
     callAjax1: function() {
